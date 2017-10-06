9775002c38db370bb99ff1c8db020eec811db6da
Yiduo Ke <yiduoke822@jhs185.org>
2017-10-06 19:30:01 -0400

    almost done?

--- app.py
+++ app.py
@@ -8,23 +8,33 @@ my_app.secret_key = 'who cares about security'
 @my_app.route('/')
 def root():
     if session.has_key("username"):
-        return render_template("loggedin.html", username = session["username"])
+        # return render_template("welcome.html", username = session["username"])
+        return redirect(url_for('welcome'))
     return render_template("login.html")
   
-@my_app.route("/login", methods=["POST"])
+@my_app.route("/login", methods=["POST","GET"])
 def login():
     if (request.form["username"]=="un" and request.form["password"]=="pw"):
         session["username"]=request.form["username"]
         session['password'] = request.form['password']
-        return render_template("loggedin.html",username=request.form["username"])
+        # return render_template("welcome.html",username=request.form["username"])
+        return redirect(url_for('welcome'))
     elif (request.form["username"]!="un"):
-        return render_template("error.html",errorMessage="wrong username")
+        return redirect(url_for("wrongUsername"))
     else:
-        return render_template("error.html",errorMessage="wrong password")
+        return redirect(url_for("wrongPassword"))
 
-# @my_app.route("/loggedin", methods=["POST"])
-# def loggedin():
-#     return render_template("loggedin.html",username=session["username"])
+@my_app.route("/wrongUsername")
+def wrongUsername():
+    return render_template("error.html",errorMessage="wrong username")
+
+@my_app.route("/wrongPassword")
+def wrongPassword():
+    return render_template("error.html",errorMessage="wrong password")
+
+@my_app.route("/welcome", methods=["POST","GET"])
+def welcome():
+    return render_template("welcome.html",username=session["username"])
 
 @my_app.route("/logout", methods=["POST"])
 def logout():
